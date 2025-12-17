637ced5135c4f757ab6c8b4e0c85b9e94260bac1
Dery Febriantara <deryfebriantara@Derys-MacBook-Pro.local>
2025-12-17 09:01:07 +0800

    chore(config): configure Astro image optimization and Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind()],
+
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+    // Enable image optimization
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+  },
+});
